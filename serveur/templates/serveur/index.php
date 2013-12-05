df242036690aaff1d07b3e17f17b926e78ce9437
nabaki <duchet.l@hotmail.fr>
2013-12-05 20:50:22 +0100

    minuscules fichiers

--- serveur/templates/serveur/index.php
+++ serveur/templates/serveur/index.php
@@ -1,6 +1,6 @@
 <?php include "includes/header.php" ?>
 <?php include "includes/navigation.php" ?>
 
-<?php include "includes/Accueil/Acceuil.php" ?>
+<?php include "includes/accueil/acceuil.php" ?>
 
 <?php include "includes/footer.php" ?>
