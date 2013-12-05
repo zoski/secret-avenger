090b6568e3c41609424c406c9f31b07de826d5a8
darkmarmotte <antoine91barthelemy@gmail.com>
2013-12-05 23:52:45 +0100

    re oubli haut.php

--- serveur/templates/serveur/includes/accueil/haut.php
+++ serveur/templates/serveur/includes/accueil/haut.php
@@ -1,5 +1,5 @@
 
-			
+
 	<p class="haut">
       <h1>Bienvenue sur Y-commerce</h1>
 	</p>
@@ -8,16 +8,9 @@
 		<li>Y-commerce est un site permettant de faire vos achats selon vos envies et critères. </li>
 		<li>Cela facilitera dans vos choix en sélectionnant les 2 ou 3 liens menant vers le produit qui correspondra le plus à vos attentes.</li>
 		<li>De plus, vous ne perdrez plus de temps avec des produits qui ne correspondent pas à ce que vous souhaitez.</li>
-		Y-commerce va simplifier le choix lors de vos achats et ceci sans marketing ciblé.</li>
+		<li>Y-commerce va simplifier le choix lors de vos achats et ceci sans marketing ciblé.</li>
 	</ul>
-
 	
-		
-		
-		
-		
-		
-
-		
-		
-
+	
+	
+	
