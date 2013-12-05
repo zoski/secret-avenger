e4b1830a5674c2868d674a16b6d246c5f64270c5
darkmarmotte <antoine91barthelemy@gmail.com>
2013-12-05 23:32:25 +0100

    beau code haut

--- serveur/templates/serveur/includes/accueil/haut.php
+++ serveur/templates/serveur/includes/accueil/haut.php
@@ -1,15 +1,17 @@
 
 			
-		<p class="haut">
-     <h1>Bienvenue sur Y-commerce</h1><br/></p>
-		 <p class="description">  <U>description:</U>
-		 Y-commerce est un site permettant de faire vos achats selon vos envies et critères.<br/> 
-		Cela facilitera dans vos choix en sélectionnant les 2 ou 3 liens menant vers le produit qui correspondra le plus à vos attentes.<br/>
-		De plus, vous ne perdrez plus de temps avec des produits qui ne correspondent pas à ce que vous souhaitez.<br/>
-		Y-commerce va simplifier le choix lors de vos achats et ceci sans marketing ciblé.<br/>
-		</p>
+	<p class="haut">
+      <h1>Bienvenue sur Y-commerce</h1>
+	</p>
+    <ul class="description">  
+		<U>description:</U>
+		<li>Y-commerce est un site permettant de faire vos achats selon vos envies et critères. </li>
+		<li>Cela facilitera dans vos choix en sélectionnant les 2 ou 3 liens menant vers le produit qui correspondra le plus à vos attentes.<br/>
+		<li>De plus, vous ne perdrez plus de temps avec des produits qui ne correspondent pas à ce que vous souhaitez.</li>
+		Y-commerce va simplifier le choix lors de vos achats et ceci sans marketing ciblé.</li>
+	</ul>
+
 	
-		
 		
 		
 		
