55293ec7592dbc445e366c766f81fde9066b477c
BCyprien <beaucreux.cyprien@gmail.com>
2013-12-06 00:12:06 +0100

    insc

---
+++ serveur/templates/serveur/confirmation.php
@@ -0,0 +1,6 @@
+<?php include "includes/header.php" ?>
+<?php include "includes/navigation.php" ?>
+	<form><h1><center> Bravo, votre inscription est reussie!</center></h1>
+	<h3><center>Bienvenue parmis nous,"pseudo"!</center></h3>
+	</form>
+<?php include "includes/footer.php" ?>
