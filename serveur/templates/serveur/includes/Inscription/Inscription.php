2dd16eee024d66c1b7ded14ec12a8beae71c2db0
BCyprien <beaucreux.cyprien@gmail.com>
2013-12-05 22:27:15 +0100

    intro

--- serveur/templates/serveur/includes/Inscription/Inscription.php
+++ serveur/templates/serveur/includes/Inscription/Inscription.php
@@ -4,7 +4,7 @@
 <br/><br/><br/>
 
 <h2><u>Veuillez remplir les champs suivants:</u></h2><br/>
-<h3>Pseudonyme:<input type="text" name="pseudo"/></h3>
+<h3>Pseudonyme:<input type="text" name="pseudo"id="pseudo" placeholder="Ex : Zozor" size="30" maxlength="10"/></h3>
 <h3>Mot de passe (qui doit comporter minimum 6 caracteres) :<input type="text"/></h3>
 <h3>Confirmation de votre mot de passe:<input type="text"/></h3>
 <h3>Adresse electronique:<input type="text"/></h3>
