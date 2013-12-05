7b6453165e99b4c0e84892fe41f0642879859a98
Gael Alberola <zoski@free.fr>
2013-12-05 23:54:04 +0100

    plop

--- serveur/templates/serveur/inscription.php
+++ serveur/templates/serveur/inscription.php
@@ -1,12 +1,26 @@
-<h1><center>Inscription</center></h1>
-<form method=post action="serveur\templates\serveur\confirmation.php">
+<form role="form">
+	 <div class="form-group">
+<h1>Inscription</h1>
+<form method="post" action="serveur\templates\serveur\confirmation.php">
 	<h2 class="inscritpion">Veuillez remplir les champs suivants:</h2>
 	<ul><!--  Formulaire d'inscription-->
-		<label><li>Pseudonyme:</li><input type="text" name="pseudo"id="pseudo" placeholder="Ex : Paul984" size="30" required/></label>
-		<label><li>Mot de passe (qui doit comporter minimum 6 caracteres) :</li><input type="password" name="pass" id="pass"required/></label>
-		<label><li>Confirmation de votre mot de passe:</li><input type="password" name="pass" id="pass"required/></label>
-		<label><li>Adresse electronique:</li><input type="email" name="pseudo" id="pseudo" placeholder="Ex : Paul984@free.fr" size="30" required/></label>
+		<label>Pseudonyme:
+			<input type="text" class="form-control" name="pseudo" id="pseudo" placeholder="Ex : Paul984" size="30" required/>
+		</label>
+
+		<label>Mot de passe (qui doit comporter minimum 6 caracteres) :
+			<input type="password" name="pass" id="pass"required/>
+		</label>
+
+		<label>Confirmation de votre mot de passe:
+			<input type="password" name="pass" id="pass"required/>
+		</label>
+	
+		<label>Adresse electronique:
+			<input type="email" name="pseudo" id="pseudo" placeholder="Ex : Paul984@free.fr" size="30" required/>
+		</label>
 	</ul>
+
 		<!-- Bouton à changer -->
-		<div style="text-align:center"><input type="submit" value="Envoyer"/></div>
+		 <button type="submit" class="btn btn-default">Envoyer</button>
 </form>
