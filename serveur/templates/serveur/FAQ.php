c5e057ed8ef6ec87dcb8aa7508ffa5cf9f3a3d5a
nabaki <duchet.l@hotmail.fr>
2013-12-06 00:46:42 +0100

    police perso

--- serveur/templates/serveur/FAQ.php
+++ serveur/templates/serveur/FAQ.php
@@ -5,34 +5,34 @@
 	<h1 >Foire Aux Questions: </h1>
 
 		<div>
-			<p class="questions" > Comment ça marche  ? </p>
+			<div class="questions" > Comment ça marche  ? </div>
 				<p >Notre site fonctionne de la manière suivante. Vous pouvez acheter ou recommander différents produits ou services. Lorsque vous êtes a la recherche d'un objet, notre site choisira les sites vendeurs les plus recommandés par les internautes, afin d'avoir le meilleur rapport qualité-prix, et le plus proche de vos désirs.</p>
 				
-			<p class="questions" > En quoi ce site diffère-t-il des autres ?</p>
+			<div class="questions" > En quoi ce site diffère-t-il des autres ?</div>
 				<p>Notre concept est unique, c'est a dire que le client est au centre de la démarche d'achat et non le produit.</p>
 				
-			<p class="questions" > Comment s'inscrire ?</p>
+			<div class="questions" > Comment s'inscrire ?</div>
 				<p >Pour vous inscrire, vous aurez besoin d'un pseudonyme et d'un mot de passe lié a celui ci, ainsi qu'une adresse e-mail valide. Vous pouvez cliquer  <a href="inscription.php" title="Cliquez ici pour vous inscrire">içi</a>  pour vous inscrire, ou simplement aller sur la page d'accueil.</p>
 				
-			<p class="questions" >Je me suis inscrit mais je ne reçois pas d'e-mail de confirmation... </p>
+			<div class="questions" >Je me suis inscrit mais je ne reçois pas d'e-mail de confirmation... </div>
 				<p >Attendez dans un premier temps quelques minutes.. Si au bout d'un certain temps vous ne recevez rien, cela signifie que l'adresse mail que vous avez fournie n'est pas valide. Veuillez recréer un compte ou dans le cas échéant, vérifier votre adresse mail.</p>
 				
-			<p class="questions" >Comment acheter ?</p>
+			<div class="questions" >Comment acheter ?</div>
 				<p> Dans la page d'accueil, vous aurez le choix entre deux possibilités. Si vous souhaitez acheter un objet, dirigez-vous vers la partie gauche (en bleue) de l'écran d'accueil. Il vous suffira ensuite de suivre la démarche a l'écran en indiquant par exemple le nom de l'objet ainsi que les critèresauxquels il doit répondre.</p>
 				
-			<p class="questions" >Vais-je acheter sur le site ? </p>
+			<div class="questions" >Vais-je acheter sur le site ? </div>
 				<p>Non, lorsque vous aurez choisi votre achat, notre site choisira pour vous le site web le plus avantageux et proche de vos désirs. Il vous suffira ensuite de cliquer sur le lien et de suivre les instructions d'achat de votre objet.</p>
 				
-			<p class="questions" >Comment recommander ?</p>
+			<div class="questions" >Comment recommander ?</div>
 				<p>Pour recommander, il suffira de vous diriger vers la partie droite de l'écran d'accueil (en rouge). Il faut au préalable posséder un compte sur notre site. Pas de panique, vous pouvez cliquer <a href="inscription.php" title="Cliquez içi pour vous inscrire">içi</a>  pour créer un compte.</p>
 				
-			<p class="questions" >Serai-je payé pour mes contributions ?</p>
+			<div class="questions" >Serai-je payé pour mes contributions ?</div>
 				<p>Non, la contribution ne peut être rémunérée pour l'instant car le site n'est qu'à sa première version. Les recommandations sont donc envoyées par les utilisateurs de manière bénévole.</p>
 
-			<p class="questions" >Je ne trouve pas de réponse a ma question , que faire ? </p>
+			<div class="questions" >Je ne trouve pas de réponse a ma question , que faire ? </div>
 				<p>Cliquez sur la rubrique Contacts et faites-nous part de votre question. Nous y répondrons dans les plus brefs délais.</p>
 
-			<p class="questions" >Je ne trouve pas de réponse a ma question , que faire ? </p>
+			<div class="questions" >Je ne trouve pas de réponse a ma question , que faire ? </div>
 				<p>Cliquez sur la rubrique Contact (<a href="contact.php" title="Cliquez içi pour nous contacter">ou içi</a> ) et faites-nous part de votre question. Nous y répondrons dans les plus brefs délais.</p>
 				
 		</div>
