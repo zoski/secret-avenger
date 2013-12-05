8936919585953797a641c3e9102da65fce14bd87
BCyprien <beaucreux.cyprien@gmail.com>
2013-12-05 22:29:10 +0100

    inscrip

--- serveur/templates/serveur/includes/Inscription/Inscription.php
+++ serveur/templates/serveur/includes/Inscription/Inscription.php
@@ -3,9 +3,14 @@
 <h1><center>Inscription</center></h1>
 <br/><br/><br/>
 
+<form method=post action="file:///C:/Users/Cyprien/Desktop/confirmation.html">
 <h2><u>Veuillez remplir les champs suivants:</u></h2><br/>
-<h3>Pseudonyme:<input type="text" name="pseudo"id="pseudo" placeholder="Ex : Zozor" size="30" maxlength="10"/></h3>
-<h3>Mot de passe (qui doit comporter minimum 6 caracteres) :<input type="text"/></h3>
-<h3>Confirmation de votre mot de passe:<input type="text"/></h3>
-<h3>Adresse electronique:<input type="text"/></h3>
+<h3><label>Pseudonyme:<input type="text" name="pseudo"id="pseudo" placeholder="Ex : Paul984" size="30" required/></label></h3>
+<h3><label>Mot de passe (qui doit comporter minimum 6 caracteres) :<input type="password" name="pass" id="pass"required/></label></h3>
+<h3><label>Confirmation de votre mot de passe:<input type="password" name="pass" id="pass"required/></label></h3>
+<h3><label>Adresse electronique:<input type="email" name="pseudo" id="pseudo" placeholder="Ex : Paul984@free.fr" size="30" required/></label></h3>
+<div style="text-align:center"><input type="submit" value="Envoyer"/></div>
+</form>
+
+
 </body>
