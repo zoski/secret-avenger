b0a61cdbcd942ddfcbbc689b0981045a92390c6b
nabaki <duchet.l@hotmail.fr>
2013-12-05 21:43:03 +0100

    correct index

--- serveur/templates/serveur/index.php
+++ serveur/templates/serveur/index.php
@@ -1,6 +1,6 @@
 <?php include "includes/header.php" ?>
 <?php include "includes/navigation.php" ?>
 
-<?php include "includes/accueil/acceuil.php" ?>
+<?php include "includes/accueil/accueil.php" ?>
 
 <?php include "includes/footer.php" ?>
