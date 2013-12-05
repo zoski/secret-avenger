1ed606fcb3ae885bf4dc2ebc45da62019ba1ca8f
Robin <denaux.robin@gmx.fr>
2013-12-06 00:20:43 +0100

    mise en place des urls

--- serveur/views.py
+++ serveur/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return render(request, 'serveur/index.html', {})
+
+def search(request, id):
+    return HttpResponse("search item %s." % id)
