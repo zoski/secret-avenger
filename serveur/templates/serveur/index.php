2760d96dd18b155e357bad67229decb1aa9746ef
nabaki <duchet.l@hotmail.fr>
2013-12-05 20:48:57 +0100

    trucs index

--- serveur/templates/serveur/index.php
+++ serveur/templates/serveur/index.php
@@ -1,6 +1,6 @@
 <?php include "includes/header.php" ?>
-<?php include "includes/nav.php" ?>
+<?php include "includes/navigation.php" ?>
 
-<?php include "includes/acceuil.php" ?>
+<?php include "includes/Accueil/Acceuil.php" ?>
 
 <?php include "includes/footer.php" ?>
