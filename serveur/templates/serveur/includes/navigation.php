0cb5e468d930606a2a60b902b90d878b0a2f1d0f
nabaki <duchet.l@hotmail.fr>
2013-12-05 21:46:08 +0100

    nav maj

--- serveur/templates/serveur/includes/navigation.php
+++ serveur/templates/serveur/includes/navigation.php
@@ -6,17 +6,15 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
 			  		</button>
-			 		<a class="navbar-brand" href="index.php">Xid</a>
+			 		<a class="navbar-brand" href="index.php">BuyOrDie</a>
 				</div>
 			
 				<div class="collapse navbar-collapse">
 			  		<ul class="nav navbar-nav">
 						<li><a href="index.php"><i class="fa fa-home fa-lg"></i> Accueil</a></li>
-						<li><a href="#event"><i class="fa fa-calendar fa-lg"></i> évènements</a></li>
-						<li><a href="#partenaires"><i class="fa fa-heart-o fa-lg"></i> Partenaires</a></li>
-						<li><a href="#contact"><i class="fa fa-comment-o fa-lg"></i> Contact</a></li> 
+						<li><a href="#event"><i class="fa fa-calendar fa-lg"></i> Inscription</a></li>
+						<li><a href="#partenaires"><i class="fa fa-heart-o fa-lg"></i> FAQ</a></li>
 			  		</ul>
 				</div><!--/.nav-collapse -->
       		</div>
