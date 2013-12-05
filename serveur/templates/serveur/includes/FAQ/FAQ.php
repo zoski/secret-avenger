5d3f3419d3b97c5a837314d25fb4a84820be0cca
Danilo971 <moco.daniel@gmail.com>
2013-12-05 20:39:12 +0100

    La routine de Daniel

--- serveur/templates/serveur/includes/FAQ/FAQ.php
+++ serveur/templates/serveur/includes/FAQ/FAQ.php
@@ -12,10 +12,10 @@
 		<h1 >Foire Aux Questions: </h1>
 			<h2>Sommaire :</h2>
 				<h3 id="comment_ça_marche">Comment ça marche  ? </h3>
-				<h3 id="comment_acheter"></h3>
-				<h3 id="comment_recommader"></h3>
-				<h3 id="comment_sinscrire"></h3>
-				<h3 id=""></h3>
+				<h3 id="comment_acheter">Comment Acheter ?</h3>
+				<h3 id="comment_recommader">Comment recommander ?</h3>
+				<h3 id="comment_sinscrire">Comment s'inscrire ?</h3>
+				<h3 id="acheter_sur_le_site">Vais-je acheter sur le site ? </h3>
 				<h3 id=""></h3>
 
 		<div>
@@ -27,7 +27,8 @@
 				<p >Pour recommander, il faut au préalable possèder un compte sur notre site. Pas de panique, vous pouvez cliquer ici (lien + bulle d'aide) pour créer un compte, cliquer ici(lien + bulle d'aide) pour obtenir de plus amples informations.</p>
 			<h3 href="#comment_sinscrire">Comment S'inscrire </h3>
 				<p >Pour s'inscrire, vous aurez besoin d'un pseudonyme et d'un mot de passe lié a celui ci, ainsi qu'une adresse e-mail valide. Vous pouvez cliquer ici (bulle d'aide + lien ) pour vous inscrire, ou simplement aller sur la page d'acceuil</p>
-			<h3 ></h3>
+			<h3 href="#acheter_sur_le_site"></h3>
+				<p> Non, lorsque vous aurez choisi votre achat, notre site choisira pour vous le site web le plus avantageux et proche de vos désirs. Il vous suffira ensuite de cliquer sur le lien et de suivre les instructions d'achat de votre objet.</p>
 			<h3 ></h3>
 
 		</div>
