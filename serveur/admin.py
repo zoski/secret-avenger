1e0036267b485dc8492a42b99cd01c7dfd23a7d3
Robin <denaux.robin@gmx.fr>
2013-12-05 23:40:26 +0100

    ajouts bdd et administration
    création des tables utilisateurs, offres et selection
    et leurs ajout a l'interface admin

--- serveur/admin.py
+++ serveur/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from serveur.models import Utilisateur, Selection, Offre
 
 # Register your models here.
+admin.site.register(Utilisateur)
+admin.site.register(Selection)
+admin.site.register(Offre)
--- serveur/models.py
+++ serveur/models.py
@@ -1,3 +1,35 @@
+
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Utilisateur(models.Model):
+    pseudo = models.ForeignKey(User, unique=True)
+    reputation = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.pseudo.username
+
+class Offre(models.Model):
+    url = models.URLField(max_length=64, blank=False)
+    nom = models.CharField(max_length=64)
+    description = models.CharField(max_length=10000)
+    nom_image = models.CharField(max_length=64)
+    prix = models.IntegerField()
+    recommandations = models.IntegerField()
+
+    def __str__(self):
+        return self.nom
+    
+
+class Selection(models.Model):
+    nom = models.CharField(max_length=64)
+    last = models.BooleanField(default=False)
+    choix1 = models.CharField(max_length=64)
+    choix2 = models.CharField(max_length=64)
+    choix3 = models.CharField(max_length=64)
+    choix4 = models.CharField(max_length=64)
+    
+    def __str__(self):
+        return self.nom
+    
