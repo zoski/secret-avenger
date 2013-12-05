755d73aa6f273a3cda2cdeaec901fb3faf17e785
darkmarmotte <antoine91barthelemy@gmail.com>
2013-12-05 22:01:14 +0100

    correction haut.php

--- serveur/templates/serveur/includes/Accueil/haut.php
+++ serveur/templates/serveur/includes/Accueil/haut.php
@@ -1,7 +1,7 @@
 
 			
 		<p class="haut">
-     <h1>Bienvenue sur Y-commerce<h1><br/></p>
+     <h1>Bienvenue sur Y-commerce</h1><br/></p>
 		 <p class="description">  <U>description:</U>
 		 Y-commerce est un site permettant de faire vos achats selon vos envies et critères.<br/> 
 		Cela facilitera dans vos choix en sélectionnant les 2 ou 3 liens menant vers le produit qui correspondra le plus à vos attentes.<br/>
