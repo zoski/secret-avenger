a1e59e4281526ba544a3e0aa226427f9ffa14edb
nabaki <duchet.l@hotmail.fr>
2013-12-05 20:44:09 +0100

    header nav footer

---
+++ serveur/templates/serveur/includes/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="Site web du Xid, le club d'informatique de l'ENSISA">
+		<meta name="authors" content="Loïc Duchet, Gaël Alberola">
+		<link rel="shortcut icon" href="ico/favicon.png">
+		<title>Xid | Club d'informatique</title>
+
+		<!-- Bootstrap core CSS + CSS perso -->
+		<link href="./css/bootstrap.css" rel="stylesheet">
+		<link href="./css/perso.css" rel="stylesheet">
+		<link href="./css/font-awesome.min.css" rel="stylesheet" >
+		<link href="./css/animations.css" rel="stylesheet">
+	</head>
---
+++ serveur/templates/serveur/includes/navigation.php
@@ -0,0 +1,23 @@
+	<body>
+		<div class="navbar navbar-inverse navbar-fixed-top">
+			<div class="container">
+				<div class="navbar-header">
+			  		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+			  		</button>
+			 		<a class="navbar-brand" href="index.php">Xid</a>
+				</div>
+			
+				<div class="collapse navbar-collapse">
+			  		<ul class="nav navbar-nav">
+						<li><a href="index.php"><i class="fa fa-home fa-lg"></i> Accueil</a></li>
+						<li><a href="#event"><i class="fa fa-calendar fa-lg"></i> Événements</a></li>
+						<li><a href="#partenaires"><i class="fa fa-heart-o fa-lg"></i> Partenaires</a></li>
+						<li><a href="#contact"><i class="fa fa-comment-o fa-lg"></i> Contact</a></li> 
+			  		</ul>
+				</div><!--/.nav-collapse -->
+      		</div>
+		</div>
