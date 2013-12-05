c719ec2683f5c45358e70075155c4b279f95307c
Danilo971 <moco.daniel@gmail.com>
2013-12-05 23:20:28 +0100

    La routine de Daniel

--- serveur/templates/serveur/includes/FAQ/FAQ.php
+++ serveur/templates/serveur/includes/FAQ/FAQ.php
@@ -15,30 +15,20 @@
 		<div>
 			<h3 href="#comment_ça_marche"> Comment ça marche  ? </h3>
 				<p >Notre site fonctionne de la manière suivante. Vous pouvez acheter ou recommander différents produits ou services.</p>
-
-			<h3 href="#difference_site" >En quoi ce site diffère-t-il des autres </h3>
-				<p></p>
-
-			<h3 href="#comment_sinscrire">Comment S'inscrire </h3>
-				<p >Pour s'inscrire, vous aurez besoin d'un pseudonyme et d'un mot de passe lié a celui ci, ainsi qu'une adresse e-mail valide. Vous pouvez cliquer ici (bulle d'aide + lien ) pour vous inscrire, ou simplement aller sur la page d'acceuil</p>
-
-			<h3 href="#probleme_mail" >Je me suis inscrit mais je ne reçois pas d'e-mail de confirmation...</h3>
-				<p></p>
-
 			<h3  href="#comment_acheter"> Comment Acheter ?</h3>
 				<p>Dans la page d'acceuil, vous aurez le choix entre deux possibilités. Si vous souhaitez acheter un objet, dirigez vous vers la partie gauche (en bleue) de l'écran d'acceuil. Il vous suffira ensuite de taper le nom de l'objet désiré.</p>
-
-			<h3 href="#acheter_sur_le_site">Vais-je acheter sur le site ? </h3>
-				<p> Non, lorsque vous aurez choisi votre achat, notre site choisira pour vous le site web le plus avantageux et proche de vos désirs. Il vous suffira ensuite de cliquer sur le lien et de suivre les instructions d'achat de votre objet.</p>
-
 			<h3 href="#comment_recommander"> Comment recommander ?</h3>
 				<p >Pour recommander, il faut au préalable possèder un compte sur notre site. Pas de panique, vous pouvez cliquer ici (lien + bulle d'aide) pour créer un compte, cliquer ici(lien + bulle d'aide) pour obtenir de plus amples informations.</p>
-			
-			
-
+			<h3 href="#comment_sinscrire">Comment S'inscrire </h3>
+				<p >Pour s'inscrire, vous aurez besoin d'un pseudonyme et d'un mot de passe lié a celui ci, ainsi qu'une adresse e-mail valide. Vous pouvez cliquer ici (bulle d'aide + lien ) pour vous inscrire, ou simplement aller sur la page d'acceuil</p>
+			<h3 href="#acheter_sur_le_site"></h3>
+				<p> Non, lorsque vous aurez choisi votre achat, notre site choisira pour vous le site web le plus avantageux et proche de vos désirs. Il vous suffira ensuite de cliquer sur le lien et de suivre les instructions d'achat de votre objet.</p>
 			<h3 href="#" ></h3>
 				<p></p>
 			<h3 href="#" ></h3>
 				<p></p>
 			<h3 href="#" ></h3>
 				<p></p>
+			<h3 href="#" ></h3>
+				<p></p>
+		</div>
