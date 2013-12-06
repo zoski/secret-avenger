6d42f429c1578d733614a896404c5a2078dd27f1
Gael Alberola <zoski@free.fr>
2013-12-06 01:49:29 +0100

    modification de confirmation

--- serveur/templates/serveur/confirmation.php
+++ serveur/templates/serveur/confirmation.php
@@ -1,6 +1,6 @@
 <?php include "includes/header.php" ?>
 <?php include "includes/navigation.php" ?>
 <div class="row">
-	<div class="alert alert-success"><i class="fa fa-check-circle fa-lg"></i><strong> Bravo !</strong> Tout c'est bien passé ;-) </div>
+	<div class="alert alert-success"><i class="fa fa-check-circle fa-lg"></i><strong> Bravo !</strong> Tout s'est bien passé ;-) </div>
 </div>
 <?php include "includes/footer.php" ?>
