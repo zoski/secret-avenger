a8ac0ca5f7dadf20b46ac60dc9476d3a463641c9
nabaki <duchet.l@hotmail.fr>
2013-12-06 01:46:26 +0100

    logo

--- serveur/templates/serveur/includes/navigation.php
+++ serveur/templates/serveur/includes/navigation.php
@@ -8,7 +8,7 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 			  		</button>
-			 		<a class="navbar-brand" href="index.php">Search&Find </a>
+			 		<a class="navbar-brand" href="xDlogo.php">Search&Find </a>
 				</div>
 			
 				<div class="collapse navbar-collapse">
---
+++ serveur/templates/serveur/xDlogo.php
@@ -0,0 +1,10 @@
+<?php include "includes/header.php" ?>
+<?php include "includes/navigation.php" ?>
+
+
+		<img src="../../static/serveur/img/logo.jpg" alt="bannière du site" />
+	<p class="lead"> "Essayez, vous allez adorer."
+	</br>Avec Search&Find l'E-commerce vient de passer de l'ère de la recherche à celle du choix.</p>
+
+
+<?php include "includes/footer.php" ?>
