7283b137068b4bf6084c52d4a1f9b0ca8029dde5
Gael Alberola <zoski@free.fr>
2013-12-06 05:04:27 +0100

    la page 404.php

---
+++ serveur/templates/serveur/404.php
@@ -0,0 +1,13 @@
+<?php include "includes/header.php" ?>
+<div class="row">
+	<div class="col-md-4 col-md-offset-4">
+		<h1>Oups !</h1>
+		<h2>Il semble qu'il n'y ai rien ici !</h2>
+		
+		<div id="object" class="slideRight">
+			<div class="alert alert-danger"><i class="fa fa-exclamation-circle fa-lg"></i> <strong>Erreur 404</strong></div>
+		</div>
+	<center><a href="index.php"><button type="button" class="btn btn-default btn-md"><i class="fa fa-rocket fa-lg"></i> Retourner au site</button></a></center>
+</div>
+
+<?php include "includes/footer.php"?>
