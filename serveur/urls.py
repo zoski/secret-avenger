b7ea5233ca5494f6531052bb32f7e1c298bb07f0
Robin <denaux.robin@gmx.fr>
2013-12-06 03:05:24 +0100

    gestion des utilisateurs
    mise en place des pages d'inscriptions et de login

--- serveur/urls.py
+++ serveur/urls.py
@@ -5,5 +5,9 @@ from serveur import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^(?P<id>\d+)/search/$', views.search, name='search'),
-    url(r'^(?P<id>\d+)/login/$', views.login, name='login'),
+    url(r'^user_crea/$', views.user_crea, name='user_crea'),
+    url(r'^inscription/$', views.inscription, name='inscription'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logingin/$', views.logingin, name='logingin'),
+                       
 )
--- serveur/views.py
+++ serveur/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User, UserManager
+from django.contrib.auth import get_user_model
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from serveur.models import Utilisateur
+
 
 # Create your views here.
 def index(request):
@@ -8,7 +14,28 @@ def index(request):
 def search(request, id):
     return HttpResponse("search item %s." % id)
 
+def user_crea(request):
+    passw = request.POST['pass']
+    name = request.POST['pseudo']
+    if (passw == request.POST['pass_conf'] and len(passw)>=6):
+        get_user_model().objects.create_user(name, request.POST['mail'], passw)
+        return HttpResponseRedirect(reverse('serveur:index'))
+    else:
+        return render(request, 'serveur/inscription.html', {
+            'error_message': "Echec de l'inscription!",
+        })
+            
+def inscription(request):
+    return render(request, 'serveur/inscription.html', {})
+
 def login(request):
-    try:
-        create_user(request.POST['pseudo'], request.POST['choice'], request.POST['choice'])
-        
+    return render(request, 'serveur/login.html', {}) 
+
+def logingin(request):
+    user = User.objects.get(username__exact=request.POST['pseudo']
+    if(user.password == request.POST['pass']):
+        return HttpResponseRedirect(reverse('serveur:index'))
+    else:
+        return render(request, 'serveur/login.html', {
+            'error_message': "Echec de l'authentification!",
+        })
