051932a31bde3d9ad0ea4ec788115bd096fa1a5d
nabaki <duchet.l@hotmail.fr>
2013-12-06 02:21:49 +0100

    chocapic logo !

--- serveur/templates/serveur/xDlogo.php
+++ serveur/templates/serveur/xDlogo.php
@@ -4,7 +4,9 @@
 <div class="row">
 	<div class="col-md-6 col-md-offset-3">
 		<div class="logo"> "Essayez, vous allez adorer." <p>
-		<img src="../../static/serveur/img/logo.jpg" alt="bannière du site" />
+			<div id="object" class="slideDown">
+			<img src="../../static/serveur/img/logo.jpg" alt="bannière du site" />
+			</div>
 		<div class="logo"> Avec Search&Find l'Y-commerce vient de passer de l'ère de la recherche à celle du choix.</p>
 	</div>
 </div>
