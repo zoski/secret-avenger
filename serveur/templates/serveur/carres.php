63de099966ba33ab5429bb6fddb3106b32f05604
nabaki <duchet.l@hotmail.fr>
2013-12-06 04:01:21 +0100

    exemple carres

---
+++ serveur/templates/serveur/carres.php
@@ -0,0 +1,33 @@
+<?php include "includes/header.php" ?>
+<?php include "includes/navigation.php" ?>
+<?php include "includes/ban.php" ?>
+
+
+			<div class="starter-template">
+				<div class="row">
+				
+					<div class="col-md-6">
+						<a href="index.php" class="btn btn-info btn-block cat active" role="button">Sportives</a>	    <!-- Ce bouton permet de rediriger vers un lien-->
+					</div>
+					
+					<div class="col-md-6">
+						<a href="index.php" class="btn btn-success btn-block cat active" role="button">Berlines</a>
+					</div>
+	
+				</div>
+				
+				<div class="row">
+				
+					<div class="col-md-6">
+						<a href="index.php" class="btn btn-danger btn-block cat active" role="button">Berlines</a>
+					</div>
+					
+					<div class="col-md-6">
+						<a href="index.php" class="btn btn-default btn-block cat active" role="button">Berlines</a>
+					</div>
+	
+				</div>
+			</div>
+
+				
+<?php include "includes/footer.php" ?>
