47f3045d87669f3eeb9fcfc38911be2eefe42f56
Gael Alberola <zoski@free.fr>
2013-12-06 01:26:14 +0100

    inscription okay

--- serveur/templates/serveur/inscription.php
+++ serveur/templates/serveur/inscription.php
@@ -3,34 +3,34 @@
 
 <form class="form-horizontal" role="form">
 		<h1>Inscription</h1>
-		<form method="post" action="serveur\templates\serveur\confirmation.php">
+		<form method="post" action="serveur\templates\serveur\login">
 		<h2 class="inscritpion">Veuillez remplir les champs suivants:</h2>
 
 	<!--  Formulaire d'inscription-->
 		 <div class="form-group">
-			<label for="inputPseudo" class="col-sm-3">Pseudonyme:</label>
-			<div class="col-sm-3">
+			<label for="inputPseudo" class="col-sm-4">Pseudonyme:</label>
+			<div class="col-sm-8">
 				<input type="text" class="form-control" name="pseudo" id="pseudo" placeholder="Ex : Paul984" size="30" required/>
 			</div>
 		</div>
 
 		<div class="form-group">
-			<label for="inputMdp" class="col-sm-3">Mot de passe (qui doit comporter minimum 6 caracteres) :</label>
-			<div class="col-sm-3">
+			<label for="inputMdp" class="col-sm-4">Mot de passe (qui doit comporter minimum 6 caracteres) :</label>
+			<div class="col-sm-8">
 				<input type="password" class="form-control"  name="pass" id="pass"required/>
 			</div>
 		</div>
 		
 		<div class="form-group">
-			<label for="inputMdp" class="col-sm-3">Confirmation de votre mot de passe:</label>
-			<div class="col-sm-3">
+			<label for="inputMdp" class="col-sm-4">Confirmation de votre mot de passe:</label>
+			<div class="col-sm-8">
 				<input type="password" class="form-control"  name="pass_conf" id="pass_conf"required/>
 			</div>
 		</div>
 
 		<div class="form-group">
-			<label for="inputEmail" class="col-sm-3">Adresse electronique:</label>
-				<div class="col-sm-3">
+			<label for="inputEmail" class="col-sm-4">Adresse electronique:</label>
+				<div class="col-sm-8">
 				<input type="email"  class="form-control" name="mail" id="mail" placeholder="Ex : Paul984@free.fr" size="30" required/>
 				</div>
 		</div>
