2eebf15893dd4bd2614038b63ccfdc75f1131116
nabaki <duchet.l@hotmail.fr>
2013-12-06 02:38:53 +0100

    indent logo

--- serveur/templates/serveur/includes/ban.php
+++ serveur/templates/serveur/includes/ban.php
@@ -1,5 +1,5 @@
 <div class="row">
-<div class="col-md-8 col-md-offset-1">
-<img src="../../static/serveur/img/banniere.jpg" alt="banniere du site" />
-</div>
+	<div class="col-md-8 col-md-offset-1">
+		<img src="../../static/serveur/img/banniere.jpg" alt="banniere du site" />
+	</div>
 </div>
