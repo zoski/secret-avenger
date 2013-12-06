e48ae0ffec7793e162a5c706516355efc23597ee
Gael Alberola <zoski@free.fr>
2013-12-06 04:42:27 +0100

    final index

--- serveur/templates/serveur/index.php
+++ serveur/templates/serveur/index.php
@@ -7,12 +7,12 @@
 			<iframe width=100% height="250px" src="//www.youtube.com/embed/vDwLtqoSCyg" frameborder="0" allowfullscreen></iframe>
 		</div>
 		<div class="col-md-7">
-			<p class="lead">
-				Search&Find est un site permettant de faire vos achats selon vos envies et critères.
+			<p class="lead"><i class="fa fa-quote-left fa-x3"></i>
+				 Search&Find est un site permettant de faire vos achats selon vos envies et critères.
 				Cela facilitera vos choix en sélectionnant les liens menant vers le produit qui correspondra le plus à vos attentes.
 			</p>	
 			<p class="lead">
-				De plus, vous ne perdrez plus de temps avec des produits qui ne correspondent pas à ce que vous souhaitez. Search&Find va simplifier le choix lors de vos achats et ceci sans marketing ciblé.
+				De plus, vous ne perdrez plus de temps avec des produits qui ne correspondent pas à ce que vous souhaitez. Search&Find va simplifier le choix lors de vos achats et ceci sans marketing ciblé. <i class="fa fa-quote-right fa-x3"></i>
 			</p>
 		</div>
 	</div>
@@ -21,17 +21,20 @@
 	<div class="row">
 		<div class="col-md-6">
 				<h2><i class="fa fa-shopping-cart"></i> Acheter</h2>
-				<p>La partie <em>acheter</em> vous permet de trouver les meilleures offres pour les produits que vous choisissez.
+				<p>La partie <strong>acheter</strong> vous permet de trouver les meilleures offres pour les produits que vous choisissez.
 					Vous choisissez les types de produits recherchés parmi les critères proposés. Le choix sera plus ciblé à chaque étape jusqu'à être 	celui que vous cherchez.
 				</p>
+				<center><a href="acheter.php"><button type="button" class="btn btn-primary btn-md">Commencer à acheter</button></a></center>
 		</div>
 
 		<div class="col-md-6">
 			<h2><i class="fa fa-users"></i> Recommander</h2>
-			<p>La partie <em>recommander</em> vous permet de conseiller à d'autres acheteurs des sites sur lesquels des produits sont disponibles.
+			<p>La partie <strong>recommander</strong> vous permet de conseiller à d'autres acheteurs des sites sur lesquels des produits sont disponibles.
 				Vous créez un profil sur lequel vous devez être connecté pour pouvoir recommander des sites. 
 				Cela vous permet de gagner des points de confiance auprès des autres utilisateurs.
 			</p>
+				<center><a href="recommander.php"><button type="button" class="btn btn-primary btn-md">Proposer</button></a></center>
+			
 		</div>
 	</div>
 	
