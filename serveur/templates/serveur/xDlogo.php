f9a554694eabf2f3411adf40f56225708e3d0fb3
nabaki <duchet.l@hotmail.fr>
2013-12-06 02:13:11 +0100

    logo ended

--- serveur/templates/serveur/xDlogo.php
+++ serveur/templates/serveur/xDlogo.php
@@ -1,10 +1,12 @@
 <?php include "includes/header.php" ?>
 <?php include "includes/navigation.php" ?>
 
-
+<div class="row">
+	<div class="col-md-6 col-md-offset-3">
+		<div class="logo"> "Essayez, vous allez adorer." <p>
 		<img src="../../static/serveur/img/logo.jpg" alt="bannière du site" />
-	<p class="lead"> "Essayez, vous allez adorer."
-	</br>Avec Search&Find l'E-commerce vient de passer de l'ère de la recherche à celle du choix.</p>
-
+		<div class="logo"> Avec Search&Find l'Y-commerce vient de passer de l'ère de la recherche à celle du choix.</p>
+	</div>
+</div>
 
 <?php include "includes/footer.php" ?>
